c0cc99820ca5c0d429e9a88feefdba040fddccd0
infosergio2020 <infosergio2020@gmail.com>
2022-01-21 17:48:59 -0300

    new file:   css/lightgallery-bundle.min.css

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-window.addEventListener("DOMContentLoaded",()=>{document.getElementById("logo").addEventListener("click",()=>toggleMenu()),document.getElementById("menuIcono").addEventListener("click",()=>toggleMenu())});const toggleMenu=()=>{document.getElementById("menuIcono").classList.toggle("active")};
+// EVENTOS PARA EL SIDEBAR DEL MENU DE CITADINE
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("logo").addEventListener("click", () => toggleMenu()), document.getElementById("menuIcono").addEventListener("click", () => toggleMenu())
+});
+const toggleMenu = () => {
+    document.getElementById("menuIcono").classList.toggle("active")
+};
+
+// LIGHT GALLERY
+var imagPop = document.getElementById("lightgallery");
+console.log(imagPop);
+lightGallery(imagPop, { 
+    download:false,
+    speed: 500,
+    plugins: [lgZoom],
+    mobileSettings:{ 
+        // controls: false, 
+        showCloseIcon: true, 
+        // download: false, 
+    }
+ });
