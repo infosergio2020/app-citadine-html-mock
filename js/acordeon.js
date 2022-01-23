5b961fbcc07401ab5a46af5189ec9d6c5b3bb56e
ori <ori_larusa@hotmail.com>
2022-01-23 18:35:01 -0300

    finish el testimonio y sus botones

--- js/acordeon.js
+++ js/acordeon.js
@@ -1,60 +1,14 @@
-/*
- *   This content is licensed according to the W3C Software License at
- *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
- *
- *   Simple accordion pattern example
- */
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-'use strict';
-
-class Accordion {
-  constructor(domNode) {
-    this.rootEl = domNode;
-    this.buttonEl = this.rootEl.querySelector('button[aria-expanded]');
-
-    const controlsId = this.buttonEl.getAttribute('aria-controls');
-    this.contentEl = document.getElementById(controlsId);
-
-    this.open = this.buttonEl.getAttribute('aria-expanded') === 'true';
-
-    // add event listeners
-    this.buttonEl.addEventListener('click', this.onButtonClick.bind(this));
-  }
-
-  onButtonClick() {
-    this.toggle(!this.open);
-  }
-
-  toggle(open) {
-    // don't do anything if the open state doesn't change
-    if (open === this.open) {
-      return;
-    }
-
-    // update the internal state
-    this.open = open;
-
-    // handle DOM updates
-    this.buttonEl.setAttribute('aria-expanded', `${open}`);
-    if (open) {
-      this.contentEl.removeAttribute('hidden');
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
     } else {
-      this.contentEl.setAttribute('hidden', '');
+      panel.style.display = "block";
     }
-  }
-
-  // Add public open and close methods for convenience
-  open() {
-    this.toggle(true);
-  }
-
-  close() {
-    this.toggle(false);
-  }
+  });
 }
-
-// init accordions
-const accordions = document.querySelectorAll('.accordion');
-accordions.forEach((accordionEl) => {
-  new Accordion(accordionEl);
-});
