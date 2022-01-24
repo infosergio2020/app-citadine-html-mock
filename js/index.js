ece6c1a06beac1a2550caef63cf30c5dce2e9dda
infosergio2020 <infosergio2020@gmail.com>
2022-01-24 12:06:02 -0300

    se corrigio el problema del video

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-import lightGallery from "https://cdn.skypack.dev/lightgallery@2.3.0-beta.4";
-
-import lgVideo from "https://cdn.skypack.dev/lightgallery@2.3.0-beta.4/plugins/video";
-
-
 // EVENTOS PARA EL SIDEBAR DEL MENU DE CITADINE
 window.addEventListener("DOMContentLoaded", () => {
     document.getElementById("logo").addEventListener("click", () => toggleMenu()), document.getElementById("menuIcono").addEventListener("click", () => toggleMenu())
@@ -19,8 +14,7 @@ if (imagPop != null){
         download:false,
         speed: 500,
         addClass: 'lg-custom-thumbnails',
-        
-        plugins: [lgZoom, lgThumbnail,lgVideo],      
+        plugins: [lgZoom, lgThumbnail, lgVideo],      
         mobileSettings:{ 
             showCloseIcon: true,
         }
