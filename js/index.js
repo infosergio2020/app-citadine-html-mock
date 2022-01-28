0f02c6f10f3745838976e3b08be3c40e9a9ef7aa
infosergio2020 <infosergio2020@gmail.com>
2022-01-28 14:56:45 -0300

    pequeños cambios en el css, corregido el problema de compilacion de funciones y variables de entorno

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // EVENTOS PARA EL SIDEBAR DEL MENU DE CITADINE
 window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("logo").addEventListener("click", () => toggleMenu()), document.getElementById("menuIcono").addEventListener("click", () => toggleMenu())
+    document.getElementById("logo").addEventListener("click", () => toggleMenu()), 
+    document.getElementById("menuIcono").addEventListener("click", () => toggleMenu())
+
 });
 const toggleMenu = () => {
     document.getElementById("menuIcono").classList.toggle("active")
