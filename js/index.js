59c068d53784d8167888a64e4c30d90c0aa51cfe
ori <ori_larusa@hotmail.com>
2022-02-07 15:40:55 -0300

    solucion casi de la barra deslizante

--- js/index.js
+++ js/index.js
@@ -8,12 +8,34 @@ window.addEventListener("DOMContentLoaded", () => {
 const toggleMenu = () => {
     document.getElementById("menuIcono").classList.toggle("active")
     document.getElementById("logo").classList.toggle("boton-active")
-    let tab_secciones=document.querySelector('#tab-secciones');
-    tab_secciones.setAttribute('tabindex', 0);
-    document.getElementById('#tab-secciones').focus();
     
-    
-    console.log(tab_secciones)
+    // let menu_deslizante=document.getElementsByClassName("sidebar-body-ul");
+    // // console.log(menu_deslizante[0].children[0])
+    // menu_deslizante[0].children.setAttribute('tabindex', 0)
+    // document.getElementById('tab-secciones').focus();
+
+
+    let lista=document.getElementsByClassName("sidebar-body-ul")[0].children;
+    if(lista[0].getAttribute('tabindex')==-1){
+        for (let i = 0; i < lista.length; i++) {
+            console.log(lista[i].getAttribute("tabindex"));
+            lista[i].setAttribute("tabindex",0);
+            document.getElementById('tab-secciones').focus();
+            console.log(lista[i].getAttribute("tabindex"));
+        }
+        
+    }
+    else{
+        for (let i = 0; i < lista.length; i++) {
+            console.log(lista[i].getAttribute("tabindex"));
+            lista[i].setAttribute("tabindex",-1);
+            document.getElementById('tab-secciones').blur();
+            console.log(lista[i].getAttribute("tabindex"));
+        }
+        // tab_secciones.setAttribute('tabindex', -1);
+        // document.getElementById('tab-secciones').blur();
+    }
+
 };
 
 // LIGHT GALLERY
